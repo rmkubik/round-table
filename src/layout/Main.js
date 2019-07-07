47f43ed3cc8181d4995d06640dadda6e01b3fc6d
Ryan Kubik <rmkubik@me.com>
2019-07-06 17:04:44 -0700

    Main layout tweaks

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -5,7 +5,9 @@ const Main = styled.main`
   display: grid;
   height: 100vh;
   grid-template-columns: 1fr 3fr;
+  grid-gap: 20px;
   box-sizing: content-box;
+  padding: 20px;
 `;
 
 export default Main;
