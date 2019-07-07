ab26b69924a48e19e1376eb97d22c3a2b3275190
Ryan Kubik <rmkubik@me.com>
2019-07-06 17:50:30 -0700

    Add card hovering

--- src/layout/Card.js
+++ src/layout/Card.js
@@ -5,6 +5,7 @@ const Card = styled.div`
   border-radius: 4px;
   padding: 25px;
   background-color: ${props => props.theme.palette.slate1};
+  cursor: pointer;
 
   h3 {
     margin-top: 0;
@@ -14,6 +15,12 @@ const Card = styled.div`
   h4 {
     margin-top: 16px;
     margin-bottom: 4px;
+  }
+
+  &:hover {
+    box-shadow: 0 5px 5px ${props => props.theme.palette.black};
+    transform: scale(1.1);
+    transition: 0.3s;
   }
 `;
 
