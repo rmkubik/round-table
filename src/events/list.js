28ae14524a8d2fb11e7c92954a72a15745374c3e
Ryan Kubik <rmkubik@me.com>
2019-07-07 18:06:44 -0700

    Create plague event

--- src/events/list.js
+++ src/events/list.js
@@ -1,9 +1,11 @@
 import dragon from "../../assets/events/dragon.yaml";
 import gondor from "../../assets/events/gondor.yaml";
+import plague from "../../assets/events/plague.yaml";
 import treason from "../../assets/events/treason.yaml";
 
 export default {
     dragon,
     gondor,
+    plague,
     treason
 };
